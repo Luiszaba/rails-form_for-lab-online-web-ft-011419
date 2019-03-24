c891319534fb013a0f6073081213274ae9124f5a
Luis Zabala <luis.zabala076@gmail.com>
2019-03-24 19:56:29 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,7 +12,7 @@ class SchoolClassesController < ApplicationController
   end
     
   def show
-    @class = SchoolClass.find(params[:id])
+    @classes = SchoolClass.find(params[:id])
   end
   
   def edit 
